4c305627a08eaf2d41119367786b9df4ff0eb8e1
Liz Ing-Simmons <liz.ingsimmons@gmail.com>
2015-12-01 12:27:55 +0000

    added facet_grid functionality

--- extra_functions.R
+++ extra_functions.R
@@ -21,17 +21,29 @@ winsorise_vec <- function(vec, lower = 0.01, upper = 0.99){
 reshape_chipprofile <- function(object){
   require(dplyr)
   require(tidyr)
-  obj_reshape <- as.data.frame(cbind(mcols(rowRanges(object)), 
-                                     as.data.frame(assays(object)[[1]])))
   
-  #get columns with data at each position - reshape these to long format
-  g_cols <- names(obj_reshape)[!(names(obj_reshape) %in% names(mcols(rowRanges(object))))]
-  obj_reshape <- gather_(obj_reshape, "xIndex", "score", g_cols)
+  gr <- rowRanges(object)
+  obj_assays <- assays(object)
+  assay_names <- metadata(object)$names
+  if (is.null(assay_names)){
+    assay_names <- 1:length(obj_assays)
+    }
+  names(obj_assays) <- assay_names
+  
+  obj_reshape_list <- lapply(assay_names, function(n){
+    df <- as.data.frame(cbind(mcols(gr), assay = n,
+                              as.data.frame(obj_assays[[n]])))
+    #get columns with data at each position - reshape these to long format
+    g_cols <- names(df)[!(names(df) %in% c(names(mcols(gr)), "assay"))]
+    df_reshape <- gather_(df, "xIndex", "score", g_cols)
+  })
+  
+  obj_reshape <- do.call("rbind", obj_reshape_list)
   return(tbl_df(obj_reshape))
 }
 
 gg_heatmap <- function(obj, x = "xIndex", y = "giID", fill = "score", 
-                       facetBy = NULL, winsorise = NULL){
+                       facetBy = NULL, facetBy_grid = NULL, winsorise = NULL){
   
   #winsorise score column
   if(!is.null(winsorise)){
@@ -46,6 +58,14 @@ gg_heatmap <- function(obj, x = "xIndex", y = "giID", fill = "score",
   
   if (!is.null(facetBy)){
     p <- p + facet_wrap(formula(paste("~",paste(facetBy,collapse="+"))),
+                        scales = "free")
+  }
+  
+  if (!is.null(facetBy_grid)){
+    if (length(facetBy_grid) != 2){
+      stop("Must supply two variables for facet_grid")
+    }
+    p <- p + facet_grid(formula(paste(facetBy_grid[[1]], "~", facetBy_grid[[2]])),
                         scales = "free")
   }
   
@@ -101,5 +121,7 @@ plot_by_range_group <- function(object, colourBy=NULL, facetBy=NULL){
 # test <- all_se_signal_list[[5]]
 # test_r <- reshape_chipprofile(test)
 # gg_heatmap(test_r, winsorise = c(0, 0.99), facet = "class")
-# gg_heatmap(test_r, winsorise = c(0, 0.99), facet = "class") + 
+# gg_heatmap(test_r, winsorise = c(0, 0.99), facetBy_grid = c("class", "assay")) + 
 #   scale_fill_viridis(option = "magma") 
+
+# object <- c(CTCF_signal[[1]], CTCF_signal[[2]])
